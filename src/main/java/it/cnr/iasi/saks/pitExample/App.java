4a172f3bd7deb4de42c807ce47e94b2e11a95f25
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2024-05-16 10:19:15 +0200

    Added a toy method (foo) in App

--- src/main/java/it/cnr/iasi/saks/pitExample/App.java
+++ src/main/java/it/cnr/iasi/saks/pitExample/App.java
@@ -6,6 +6,18 @@ package it.cnr.iasi.saks.pitExample;
  */
 public class App 
 {
+    public void foo( String str ){
+        String outStr;
+        if ((str.length()%2)==0){
+            outStr="even-";        
+        } else {
+            outStr="odd-";        
+        }
+        outStr=outStr+str;
+        
+        System.out.println( "This is the string manipulated by foo:"+ outStr);
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
